a5671824d6b9767f4acbd2777151a23eede21c8b
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-11-21 08:05:37 +0100

    [add] input

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Input } from './components/forms/Input.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [search, setSearch] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="box">
+			<div className="top">
+				<Input value={search} onChange={setSearch} placeholder="Rechercher..." />
+			</div>{' '}
+		</div>
+	);
 }
 
-export default App
+export default App;
---
+++ src/components/forms/Input.jsx
@@ -0,0 +1,5 @@
+export function Input({ placeholder, value, onChange }) {
+	return (
+		<input type="text" className="form-control" value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} />
+	);
+}
