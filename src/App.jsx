0eef78db788508fec39c80b9c5c6ea8596004374
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-11-27 10:36:17 +0100

    [fix] app and input

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,26 @@ import { CardList } from './components/main/CardList.jsx';
 function App() {
 	const [search, setSearch] = useState('');
 
-	const [completeList, setCompleteList] = useState(['a', 'abbr', 'acronym', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'image', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'marquee', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'nobr', 'noembed', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'plaintext', 'portal', 'pre', 'progress', 'q', 'rb', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'search', 'section', 'select', 'slot', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr', 'xmp']);
-	const [list, setList] = useState(['a', 'abbr', 'acronym', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br']);
+	const [completeList, setCompleteList] = useState(['a', 'abbr', 'acronym', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base', 'bdi', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'image', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'marquee', 'menu', 'menuitem', 'meta', 'meter', 'nav', 'nobr', 'noembed', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'plaintext', 'portal', 'pre', 'progress', 'q', 'rb', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'search', 'section', 'select', 'slot', 'small', 'source', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr', 'xmp']);
+	const [list, setList] = useState([]);
 
-	console.log(list);
+	let isEmpty = false
+
+	if (completeList.includes(search)) {
+		if (!list.includes(search)) {
+			setList([...list, search]);
+			isEmpty = true
+			setSearch("")
+		}
+	}
+	
 	return (
 		<>
 			<span role="group" style={{ display: 'flex' }}>
-				<Input value={search} onChange={setSearch} placeholder="Rechercher..." />
+				<Input value={search} onChange={setSearch} placeholder="Rechercher..." empty={isEmpty}/>
 				<Number value={list.length} totalValue={completeList.length} />
 			</span>
-			<ContainerList value={completeList} />
+			<ContainerList value={list} />
 		</>
 	);
 }
@@ -29,7 +38,7 @@ function ContainerList({ value }) {
 	}
 
 	return (
-		<article style={{ padding: '0px', display: 'flex', overflow: 'hidden' }}>
+		<article id="box">
 			<div style={{ display: 'flex', flexWrap: 'wrap' }}>{elementList}</div>
 		</article>
 	);
--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,5 +1,11 @@
-export function Input({ placeholder, value, onChange }) {
-	return (
-		<input type="text" className="form-control" value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} />
-	);
+export function Input({ placeholder, value, onChange, empty}) {
+
+	if (empty) {
+		return <input type="text" className="form-control" value="{value}" placeholder={placeholder} onChange={(e) => onChange("")} />;
+	}
+	else{
+		return <input type="text" className="form-control" value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} />;
+	}
+
+	
 }
