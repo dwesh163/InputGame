58f49aedc4937a599de0511ba8484f3543d733b9
Azecko <samikarin24@gmail.com>
2023-12-08 00:03:55 +0100

    [refactor] `empty` is not necessary anymore
    With the way I refactored the code, `empty` and `isEmpty` were not necessary anymore.

--- src/components/forms/Input.jsx
+++ src/components/forms/Input.jsx
@@ -1,7 +1,3 @@
-export function Input({ placeholder, value, onChange, empty }) {
-    if (empty) {
-        return <input id="input" type="text" className="form-control" value="{value}" placeholder={placeholder} onChange={(e) => onChange('')} />;
-    } else {
-        return <input id="input" type="text" className="form-control" value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} />;
-    }
+export function Input({ placeholder, value, onChange }) {
+    return <input id="input" type="text" className="form-control" value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} />;
 }
