d926741e8bc2160a9c54915bae1433e24794dee1
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-12-08 14:54:50 +0100

    [add] timer text

--- src/components/NavComponents.jsx
+++ src/components/NavComponents.jsx
@@ -1,4 +1,8 @@
+import { Timer } from './forms/Timer.jsx';
+
 export function NavComponents({ onClick, isSettings }) {
+    const settings = JSON.parse(localStorage.getItem('inputGameSettings'))[localStorage.getItem('inputGameCurrent')];
+
     return (
         <nav style={{ padding: '0px 30px', marginTop: '15px' }}>
             <div className="logo">
@@ -7,6 +11,9 @@ export function NavComponents({ onClick, isSettings }) {
                     <h1 className="text-white fw-bold fst-italic">InputGame</h1>
                 </div>
             </div>
+
+            {!isSettings ? settings['timer'] ? <Timer /> : <></> : <></>}
+
             <div className="settingLogo" onClick={onClick}>
                 {isSettings ? <img src="./exit.svg" alt="" /> : <img src="./settings.svg" alt="" />}
             </div>
---
+++ src/components/forms/Timer.jsx
@@ -0,0 +1,3 @@
+export function Timer() {
+    return <h1>00:15:00</h1>
+}
