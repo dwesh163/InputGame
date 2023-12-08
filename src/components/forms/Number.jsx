42e50ad480312d90d164632f36f0a495d2e949e3
Azecko <samikarin24@gmail.com>
2023-12-08 10:46:31 +0100

    [refactor] More readable Number.jsx
    The line was a bit long, so I made every attribute into another line so the line is tinyer.

--- src/components/forms/Number.jsx
+++ src/components/forms/Number.jsx
@@ -1,3 +1,8 @@
 export function Number({ value, totalValue }) {
-    return <input style={{ width: '30%', textAlign: 'center' }} type="text" value={`${value} / ${totalValue}`} aria-label="Read-only input" readOnly />;
+    return <input
+        style={{ width: '30%', textAlign: 'center' }}
+        type="text" value={`${value} / ${totalValue}`}
+        aria-label="Read-only input"
+        readOnly
+    />;
 }
