535574d8b96effd1d22abd5eac6b1254f26dd17e
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-12-11 11:18:18 +0100

    [fix] click on card

--- src/components/NavComponents.jsx
+++ src/components/NavComponents.jsx
@@ -1,10 +1,9 @@
 export function NavComponents({ onClick, isSettings, isWelcome }) {
-
     const url = new URL(window.location.href);
 
     return (
         <nav style={{ padding: '0px 30px', marginTop: '15px' }}>
-            <div className="logo" onClick={e => (window.location.href=url.origin + url.pathname)}>
+            <div className="logo" onClick={(e) => (window.location.href = url.origin + url.pathname)}>
                 <div style={{ display: 'flex' }}>
                     <img src="./logo.png" alt="" />
                     <h1 className="text-white fw-bold fst-italic">InputGame</h1>
