a4609c8642be6a15562bf7c2777ecd993834797d
Dwesh163 <ehouarn.duriaux@protonmail.com>
2023-12-12 11:29:13 +0100

    [update] name repository

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/InputGameReact/', // see https://vitejs.dev/guide/static-deploy#github-pages
+  base: '/InputGame/', // see https://vitejs.dev/guide/static-deploy#github-pages
   plugins: [react()],
   server: {
     host: true,
